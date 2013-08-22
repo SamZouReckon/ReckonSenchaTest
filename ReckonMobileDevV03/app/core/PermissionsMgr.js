1377a3d272177423cfc0646d6e322e0dea15818e
Gary Page-Wood <garyp@aps-advance.com>
2013-08-21 20:26:44 -0500

    Added more fine-grained control of permissions overrides.

--- ReckonMobileDevV03/app/core/PermissionsMgr.js
+++ ReckonMobileDevV03/app/core/PermissionsMgr.js
@@ -7,14 +7,22 @@ Ext.define('RM.core.PermissionsMgr', {
 	},
 	
 	setPermissions: function(permissions){
-        this.permissions = permissions;
+        this.permissions = permissions.map(function(action) { return action.toLowerCase() });
         this.triggerUpdateEvent();
     },
     
     getPermission: function(actionCode){        
-        var granted = this.permissions.indexOf(actionCode) > -1;
+        var granted = this.permissions.indexOf(actionCode.toLowerCase()) > -1;
         //<debug>
-        if ("overrideAccessValue" in this) { return this.overrideAccessValue; }        
+        if (actionCode in this.getOverrides()) { 
+            console.log("Action override of '" + this.overrides[actionCode] + "' applied to: " + actionCode);
+            return this.overrides[actionCode]; 
+        }
+        if ("globalOverride" in this) { 
+            console.log("Global override of '" + this.globalOverride + "' applied to: " + actionCode);
+            return this.globalOverride; 
+        }        
+        console.log("Access value of '" + granted + "' applied to: " + actionCode);
         //</debug>
         return granted;
     },
@@ -45,20 +53,37 @@ Ext.define('RM.core.PermissionsMgr', {
     
     //<debug>
     ,
-    itIsWhatItIs: function() {
-        delete this.overrideAccessValue;
+    resetPermissions: function() {
+        delete this.globalOverride;
+        delete this.overrides;
         this.triggerUpdateEvent();
     },
     
-    itsAllGood: function() {
-        this.overrideAccessValue = true;        
+    allowEveryThing: function() {
+        this.globalOverride = true;        
         this.triggerUpdateEvent();
     },
 
-    itsAllBad: function() {
-        this.overrideAccessValue = false;        
+    allowNothing: function() {
+        this.globalOverride = false;        
         this.triggerUpdateEvent();
-    }
+    },
+    
+    alwaysAllowAction: function(action) {
+        this.getOverrides()[action] = true;    
+        this.triggerUpdateEvent();
+    },
+    
+    alwaysDenyAction: function(action) {
+        this.getOverrides()[action] = false;    
+        this.triggerUpdateEvent();
+    },
+    
+    getOverrides : function() {    
+        if(!this.overrides) {this.overrides = {};}   
+        return this.overrides;
+    }    
+    
     //</debug>
 	
 });

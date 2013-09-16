beea14c5115996f74b6c41088c87d0357f23bf76
Rajesh Rathod <rajeshr@aps-advance.co.nz>
2013-09-16 00:09:29 -0500

    RMAmountFIeld with restrictions and field value formatting on field blur (keyboard hide event)

--- ReckonMobileDevV03/app/component/RMAmountField.js
+++ ReckonMobileDevV03/app/component/RMAmountField.js
@@ -43,7 +43,11 @@ Ext.define('RM.component.RMAmountField', {
         });
     },
     
-    removeKeypad: function(field) {          
+    removeKeypad: function(field) { 
+        var val = this.getValue();
+        if (this.config.decimalPlaces && val != '')
+            this.setValue(this.getPrefix() + Ext.Number.toFixed(parseFloat(val), this.config.decimalPlaces));        
+        
         if (this.keypadOnTop) {            
             this.owningPanel.remove(this.keypad);          
             this.keypadOnTop = false;
@@ -52,50 +56,58 @@ Ext.define('RM.component.RMAmountField', {
     
     onKeyTap: function (key) {        
         var val = this.getValue();
-        var valStr = this.config.prefix + val.toString();
+        var valStr = this.getPrefix() + val.toString();
         var valStrLen = valStr.length;  
-        
+        var pointIndex = valStr.indexOf('.');
+               
         if (key === 'back') {
             if (valStrLen > 0) {
                 valStr = valStr.slice(0, -1);
                 this.setValue(valStr);                
-            }
-            return;
-        }  
-        if(val === null || val === undefined ) {            
-            return;
-        }
-        if(this.config.decimalPlaces && valStr.indexOf('.') != -1){
-            if(key === '.') return;
-            if( valStrLen-1 < ( valStr.indexOf('.') + this.config.decimalPlaces ) ){
-                this.setValue(valStr + key);
-            }
-        }
-        else{
-            this.setValue(valStr + key);
+            }            
         } 
+        else {
+            if (this.config.decimalPlaces) {
+                if ((key != '.' && valStrLen - 1 < pointIndex + this.config.decimalPlaces) || pointIndex < 0) {
+                    if (valStr == this.getPrefix() + '0' && key != '.')
+                        valStr = this.getPrefix();                
+                    this.setValue(valStr + key);
+                }
+            }
+            else {                
+                if (pointIndex != -1 && key == '.')
+                    return;
+                if (valStr == this.getPrefix() + '0')
+                    valStr = this.getPrefix(); 
+                this.setValue(valStr + key);
+            } 
+        }
     },   
     
     //Overridden getValue to remove prefix
-    getValue: function(){
+    getValue: function() {
         var val = this.callParent();
         var valStr = val.toString();        
-        if(this.config.prefix && valStr.indexOf(this.config.prefix) != -1){
-            valStr = valStr.slice(this.config.prefix.length);            
+        if (this.config.prefix && valStr.indexOf(this.config.prefix) != -1) {
+            valStr = valStr.slice(this.getPrefix().length);            
         }        
         return valStr;
     },
     
-    handleTapInEntireScreen: function(e){        
-        if(this.fieldUniqueId !== e.target.id){           
+    handleTapInEntireScreen: function(e) {        
+        if (this.fieldUniqueId !== e.target.id) {           
             this.removeKeypad();
         }        
     },    
     
-    getRootContainer: function(owningPanel){
-        while(owningPanel.getParent()){  
+    getRootContainer: function(owningPanel) {
+        while (owningPanel.getParent()) {  
             owningPanel = owningPanel.getParent();
         }
         return owningPanel;
+    },
+    
+    getPrefix: function() {
+        return (this.config.prefix ? this.config.prefix : '');
     }
 });

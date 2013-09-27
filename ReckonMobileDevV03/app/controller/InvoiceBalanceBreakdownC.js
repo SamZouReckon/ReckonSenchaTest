558ee88362a2cd60d66ee1ccdf6263cb9a493ff8
Gary Page-Wood <garyp@aps-advance.com>
2013-09-26 21:42:39 -0500

    Task 361: reorder balance breakdown to logically follow the claculation like the web app does.

--- ReckonMobileDevV03/app/controller/InvoiceBalanceBreakdownC.js
+++ ReckonMobileDevV03/app/controller/InvoiceBalanceBreakdownC.js
@@ -32,30 +32,38 @@ Ext.define('RM.controller.InvoiceBalanceBreakdownC', {
         console.log(Ext.encode(data));
         
         this.getBreakdownCont().setHtml(Ext.util.Format.format(
-                   '<table width="90%" class="rm-tablelayout">' +
-                    '<tr>'+
-                   '<div class="rm-balance-breakdown-row"><span>Total</span><span class="rm-balance-breakdown-amount">${0}</span></div>' +
-                   '</tr>' +
-                    '<tr>' +
-                   '<div class="rm-balance-breakdown-row"><span>Discount</span><span class="rm-balance-breakdown-amount">${1}</span></div>' +
-                   '</tr>' +
-                    (data.AmountTaxStatus !== RM.Consts.TaxStatus.NON_TAXED ? 
-                    '<tr>' +
-                   '<div class="rm-balance-breakdown-row"><span>Tax</span><span class="rm-balance-breakdown-amount">${2}</span></div>' +
-                   '</tr>' 
-                    : '') +
-                    '<tr>' +
-                   '<div class="rm-balance-breakdown-row"><span>Subtotal</span><span class="rm-balance-breakdown-amount">${3}</span></div>' +
-                   '</tr>' +
-                    '<tr>' +
-                   '<div class="rm-balance-breakdown-row"><span>Already paid</span><span class="rm-balance-breakdown-amount">${4}</span></div>' +
-                   '</tr>' +
-                    '<tr>' +
-                   '<div class="rm-balance-breakdown-due"><span>Balance due</span><span class="rm-balance-breakdown-amount">${5}</span></div>'   +
-                   '</tr>' + 
-                   '</table>'              
-                   , this.formatNumber(data.Amount), this.formatNumber(data.DiscountTotal), this.formatNumber(data.Tax), this.formatNumber(data.Subtotal), this.formatNumber(data.Paid), this.formatNumber(data.BalanceDue))
-                   );
+            '<table width="90%" class="rm-tablelayout">' +        
+                '<tr>' +
+                    '<div class="rm-balance-breakdown-row"><span>Subtotal</span><span class="rm-balance-breakdown-amount">${0}</span></div>' +
+                '</tr>' +
+                '<tr>' +
+                    '<div class="rm-balance-breakdown-row"><span>Discount</span><span class="rm-balance-breakdown-amount">${1}</span></div>' +
+                '</tr>' +
+                (data.AmountTaxStatus === RM.Consts.TaxStatus.NON_TAXED ? '' :
+                '<tr>'+
+                    '<div class="rm-balance-breakdown-row"><span>Total (excluding tax)</span><span class="rm-balance-breakdown-amount">${2}</span></div>' +
+                '</tr>' +                
+                '<tr>' +
+                    '<div class="rm-balance-breakdown-row"><span>Tax</span><span class="rm-balance-breakdown-amount">${3}</span></div>' +
+                '</tr>') +
+                '<tr>'+
+                    '<div class="rm-balance-breakdown-row"><span>Total</span><span class="rm-balance-breakdown-amount">${4}</span></div>' +
+                '</tr>' +
+                '<tr>' +
+                    '<div class="rm-balance-breakdown-row"><span>Already paid</span><span class="rm-balance-breakdown-amount">${5}</span></div>' +
+                '</tr>' +
+                '<tr>' +
+                    '<div class="rm-balance-breakdown-due"><span>Balance due</span><span class="rm-balance-breakdown-amount">${6}</span></div>'   +
+                '</tr>' + 
+             '</table>', 
+            this.formatNumber(data.Subtotal), 
+            this.formatNumber(data.DiscountTotal), 
+            this.formatNumber(data.AmountExTax), 
+            this.formatNumber(data.Tax), 
+            this.formatNumber(data.Amount), 
+            this.formatNumber(data.Paid), 
+            this.formatNumber(data.BalanceDue))
+        );
     },
 
     formatNumber: function(val){

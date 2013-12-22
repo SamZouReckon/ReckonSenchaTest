c24a9e752ef176e2d684558570272c812093433f
Gary Page-Wood <garyp@aps-advance.com>
2013-12-22 13:54:34 -0600

    hmm

--- ReckonMobileDevV04/Plugins/cordova-plugin-redraw/www/redraw-plugin.js
+++
@@ -1,9 +0,0 @@
-var exec = require("cordova/exec");
-
-var Redraw = function() {};
-
-Redraw.invalidateWebView = function (successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Redraw", "invalidateWebView", []);
-};
-
-module.exports = Redraw;

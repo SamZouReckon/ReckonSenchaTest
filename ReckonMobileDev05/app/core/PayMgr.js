daa76de55ce32053d5a8675bcf34de34e77c1ba4
Travis Beesley <travis.beesley@reckon.com.au>
2014-02-24 19:45:07 -0600

    Changes made by Travis Beesley (travis.beesley@reckon.com.au).

--- ReckonMobileDev05/app/core/PayMgr.js
+++ ReckonMobileDev05/app/core/PayMgr.js
@@ -9,7 +9,46 @@ Ext.define('RM.core.PayMgr', {
     
     showScreen: function (screenName, screenData, callBack, callBackScope) {   
         RM.AppMgr.showScreen.apply(this, arguments); 
+    },
+    
+    createTransaction: function(transaction, callBack, callBackScope)
+    {
+        RM.AppMgr.saveServerRec('PayTransaction', true, transaction,
+		    function () {                    
+		        callBack.call(callBackScope);
+		    },
+		    this,
+            function(recs, eventMsg){
+                RM.AppMgr.showOkMsgBox(eventMsg);
+            }
+	    );
+    },
+    
+    getTransaction: function(transactionId, callBack, callBackScope){
+        RM.AppMgr.getServerRec('PayTransaction', true, transactionId,
+		    function () {                    
+		        callBack.call(callBackScope);
+		    },
+		    this,
+            function(recs, eventMsg){
+                RM.AppMgr.showOkMsgBox(eventMsg);
+            }
+	    );
+    },
+    
+    sendReceipt: function(receipt, callBack, callBackScope){
+        
+    },
+    
+    createRefund: function(refund, callBack, callBackScope){
+        
+    },
+    
+    createTransactionHistory: function(transactionHistory, callBack, callBackScope){
+        
+    },
+    
+    readTransactionHistory: function(transactionHistoryId, callBack, callBackScope){
+        
     }
-    
-    
 });

d362bceeda76407967babe667cd516e79b60f6ce
Travis Beesley <travis.beesley@reckon.com.au>
2014-04-23 22:32:51 -0500

    Pay Device fix

--- ReckonMobileDev05/Plugins/paydevice/src/ios/PayDevice.h
+++ ReckonMobileDev05/Plugins/paydevice/src/ios/PayDevice.h
@@ -4,6 +4,7 @@
 @interface PayDevice : CDVPlugin <MPEinteractionDelegate>
 
 - (void)doTransaction:(CDVInvokedUrlCommand*)command;    
+- (void)MPEinteractionEvent: (MPEbaseInteraction*) interaction;
 //- (void)isSecureDeviceConnected:(CDVInvokedUrlCommand*)command;
 
 @property (nonatomic, strong) NSNumber *amount;

1e6c7bf32e76f44b3e97056e1325302dac4a25ad
Travis Beesley <travis.beesley@reckon.com.au>
2014-06-30 17:47:49 -0500

    Changes made by Travis Beesley (travis.beesley@reckon.com.au).

--- ReckonMobileDev05/app/core/AppMgr.js
+++ ReckonMobileDev05/app/core/AppMgr.js
@@ -409,7 +409,7 @@ Ext.define('RM.core.AppMgr', {
             this.baseApiUrl = 'http://r1mobileqa.reckon.com.au/api';            
         }
         else if(apiLocation == 'devlocaliis'){
-            this.baseApiUrl = 'http://localhost:/Reckon.Host.ReckonOneMobile/api';
+            this.baseApiUrl = 'http://localhost/api';
         }        
         else {
             this.baseApiUrl = '/api';
